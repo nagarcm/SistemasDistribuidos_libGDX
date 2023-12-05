0f57ba98991d190e2cc5b001b2e35654a5f9b018
nagarcm <nagarcm@CCTS2-25.unirioja.loc>
2023-12-05 18:52:42 +0100

    InitialCommit

---
+++ SitemasDistribuidos_GDX-desktop/src/distribuidos/slaythespire/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package distribuidos.slaythespire;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import distribuidos.slaythespire.Principal;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("SitemasDistribuidos_GDX");
+		new Lwjgl3Application(new Principal(), config);
+	}
+}
